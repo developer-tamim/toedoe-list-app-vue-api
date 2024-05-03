f98276d28edd52a0662f0e2b587e3ac089d5589f
developer-tamim <tawsiftamim24@gmail.com>
2024-05-03 14:50:32 +0600

    inline update task

--- src/components/tasks/Task.vue
+++ src/components/tasks/Task.vue
@@ -1,52 +1,65 @@
 <template>
-    <li class="list-group-item py-3">
+  <li class="list-group-item py-3">
+    <div class="d-flex justify-content-start align-items-center">
+      <input
+        class="form-check-input mt-0"
+        :class="completedClass"
+        type="checkbox"
+        :checked="task.is_completed"
+      />
 
-        <div class="d-flex justify-content-start align-items-center">
+      <div
+        class="ms-2 flex-grow-1"
+        :class="completedClass"
+        title="Double click the text to edit or remove"
+        @dblclick="($event) => (isEdit = true)"
+      >
 
-            <input class="form-check-input mt-0" :class="completedClass" type="checkbox" :checked="task.is_completed" />
+      
+        <div class="relative" v-if="isEdit">
+          <input
+            class="editable-task"
+            type="text"
+            @keyup.esc="($event) => (isEdit = false)"
+            v-focus
+            @keyup.enter="updateTask"
+          />
 
-            <div 
-            class="ms-2 flex-grow-1" :class="completedClass" 
-            title="Double click the text to edit or remove"
-            @dblclick="$event => isEdit = true"
-            >
-
-                <div class="relative" v-if="isEdit">
-
-                    <input class="editable-task" type="text" @keyup.esc="$event => isEdit = false" v-focus/>
-
-                </div>
-
-                <span v-else>{{ task.name }}</span>
-
-            </div>
-
-            <!-- <div class="task-date">24 Feb 12:00</div> -->
 
         </div>
 
-        <TaskAction @edit="$event => isEdit = true" v-show="!isEdit"/>
+        <span v-else>{{ task.name }}</span>
+      </div>
 
-    </li>
+      <!-- <div class="task-date">24 Feb 12:00</div> -->
+    </div>
+
+    <TaskAction @edit="($event) => (isEdit = true)" v-show="!isEdit" />
+  </li>
 </template>
 
 <script setup>
-import { computed, ref } from 'vue';
+import { computed, ref } from "vue";
 
-import TaskAction from '../tasks/TaskAction.vue';
+import TaskAction from "../tasks/TaskAction.vue";
 
 const props = defineProps({
-    task: Object
-})
+  task: Object,
+});
 
-const isEdit = ref(false)
+const emit = defineEmits(['updated'])
 
-const completedClass = computed(() => props.task.is_completed ? "completed" : "")
+const isEdit = ref(false);
+
+const completedClass = computed(() => props.task.is_completed ? "completed" : "");
 
 const vFocus = {
-    mounted: (el) => el.focus()
+  mounted: (el) => el.focus(),
+};
+
+const updateTask = event => {
+    const updateTask = {...props.task, name: event.target.value}
+    isEdit.value = false
+    emit('updated', updateTask)
 }
-
-
 </script>
-
--- src/pages/TasksPage.vue
+++ src/pages/TasksPage.vue
@@ -9,7 +9,7 @@
 
             <!-- List of uncompleted tasks -->
 
-            <Tasks :tasks="uncompletedTasks" />
+            <Tasks :tasks="uncompletedTasks" @updated="handleUpdatedTask" />
 
             <!-- show toggle button -->
             <div class="text-center my-3" v-show="showToggleCompletedBtn">
@@ -36,7 +36,7 @@
 
 <script setup>
 import { computed, onMounted, ref } from "vue";
-import { allTasks, createTask } from "../http/task-api";
+import { allTasks, createTask, updateTask } from "../http/task-api";
 import Tasks from "../components/tasks/Tasks.vue";
 import NewTask from "../components/tasks/NewTask.vue";
 
@@ -71,4 +71,13 @@ const handleAddedTask = async (newTask) => {
 
   tasks.value.unshift(createdTask.data);
 };
+
+const handleUpdatedTask = async (task) => {
+ const {data: updatedTask} = await updateTask(task.id, {
+    name: task.name
+  })
+
+  const currentTask = tasks.value.find(item => item.id === task.id);
+  currentTask.name = updatedTask.data.name;
+}
 </script>
